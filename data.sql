be239c0225a5a5b81c15b594c2ce3d62c63bf622
tasyamarbun <tasyaaprilda21@gmail.com>
2026-03-01 16:12:18 +0700

    init

--- data.sql
+++ data.sql
@@ -8,3 +8,18 @@ CREATE TABLE IF NOT EXISTS plants (
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 );
+
+
+CREATE TABLE IF NOT EXISTS bags (
+                                    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    nama VARCHAR(100) NOT NULL,
+    merek VARCHAR(100) NOT NULL,
+    harga DOUBLE PRECISION NOT NULL DEFAULT 0,
+    warna VARCHAR(50) NOT NULL,
+    bahan VARCHAR(100) NOT NULL,
+    stok INTEGER NOT NULL DEFAULT 0,
+    deskripsi TEXT NOT NULL,
+    path_gambar VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+    );
